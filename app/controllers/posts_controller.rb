411ce2464b83060893ee71af308790d9c1a0990d
jfsoza <juan.soza@kellogg.northwestern.edu>
2023-02-21 05:57:50 +0000

    final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,13 @@
 class PostsController < ApplicationController
 
-    def index
-        @posts = Post.all
-    end
-   
+
     
     def new
         @post = Post.new
         @post ["place_id"] = params["place_id"]
     end
     
-    def show
-        @post = Post.find_by({ "id" => params["id"] })
-        @place = Place.find_by({ "id" => @post["place_id"] })
-    end
+
 
     def create
         @post = Post.new
